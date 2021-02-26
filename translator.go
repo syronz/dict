ec245964d5a181b11c29e656a90716b15936d200
Diako Amir <sabina.diako@gmail.com>
2021-02-26 21:25:50 +0300

    first commit

---
+++ translator.go
@@ -0,0 +1,63 @@
+package dict
+
+import (
+	"fmt"
+)
+
+// SafeTranslate doesn't add !!! around word in case of not exist for translate
+func SafeTranslate(str string, lang Lang, params ...interface{}) (string, bool) {
+	if !translateInBackend {
+		return str, true
+	}
+
+	term, ok := thisTerms[str]
+	if ok {
+		var pattern string
+
+		switch lang {
+		case En:
+			pattern = term.En
+		case Ku:
+			pattern = term.Ku
+		case Ar:
+			pattern = term.Ar
+		default:
+			pattern = str
+		}
+
+		// if type of param is dict.R then translate it
+		for i, v := range params {
+			switch v.(type) {
+			case R:
+				term := v.(R)
+				params[i] = T(string(term), lang)
+			}
+		}
+
+		if params != nil {
+			if !(params[0] == nil || params[0] == "") {
+				pattern = fmt.Sprintf(pattern, params...)
+			}
+		}
+
+		return pattern, true
+
+	}
+
+	return "", false
+
+}
+
+// T the requested term
+func T(str string, lang Lang, params ...interface{}) string {
+	if !translateInBackend {
+		return str
+	}
+
+	pattern, ok := SafeTranslate(str, lang, params...)
+	if ok {
+		return pattern
+	}
+
+	return "!!! " + str + " !!!"
+}
